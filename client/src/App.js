73bb6e81fb8ac4ee988529c3cbd1677ea8fbd573
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-06-07 15:13:11 +0100

    front-end done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import io from 'socket.io-client'
+import TextField from '@material-ui/core/TextField'
 import './App.css';
 
+const socket = io.connect('http://localhost:4000')
+
 function App() {
+  const [state, setState] = useState({message: '', name: ''})
+  const [chat, setChat] = useState([])
+
+  const handleChange = (e) => {
+    setState({...state, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const renderChat = () => {
+    return chat.map(({message, name}, i) => (
+      <div key={i}>
+        <h3>
+          {name} <span>{message}</span>
+        </h3>
+      </div>)
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <form onSubmit = {handleSubmit}>
+        <div className="name-field">
+          <TextField
+            name="name"
+            onChange = {handleChange}
+            value = {state.name}
+            label='Name'
+          />
+        </div>
+        <div>
+          <TextField
+            name = "message"
+            onChange = {handleChange}
+            value = {state.message}
+            id = "outlined-multiline-static"
+            variant = "outlined"
+            label='Message'
+          />
+        </div>
+        <button>Send Message</button>
+      </form>
+      <div className="render-chat">
+        <h1>Chat Log</h1>
+        {renderChat()}
+      </div>
     </div>
   );
 }
